7fba95f9f85c02b759532d5632b7750f1f7ecdac
choijeaho86@gmail.com <choijeaho86@gmail.com>
2021-05-15 11:11:01 +0900

    delete added

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
+import { stringify } from 'querystring';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -8,16 +9,19 @@ export class ProductsController {
 
     }
 
+    // 모든 상품의 정보를 받는다.
     @Get()
     getAllProducts(){
         return {product : this.productsService.getProducts()};
     }
  
+    // 주소지에 있는 id의 상품을 찾는다.
     @Get(':id')
     getProduct(@Param('id') prodId: string){
         return this.productsService.getSingleProduct(prodId);
     }
 
+    // 상품을 추가한다.
     @Post()
     addProduct(
         // 전체의 Body를 받기위해서 아래 주석을 사용할수 있음
@@ -34,5 +38,27 @@ export class ProductsController {
          };
 
     }
+
+    // 존재하는 상품을 수정한다.
+    @Patch(':id')
+    updateProduct(
+        @Param('id') prodId : string,
+        @Body('title') prodTitle : string,
+        @Body('description') prodDesc : string,
+        @Body('price') prodPrice : number
+    ){
+        
+        this.productsService.updateProduct(prodId,prodTitle,prodDesc,prodPrice);
+        return null;
+    }
+
+    // 존재하는 상품을 삭제한다.
+    @Delete(':id')
+    deleteProduct(@Param('id') prodId : string){
+        this.productsService.deleteProduct(prodId);
+        return "delete";
+    }
+
+
 
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,7 +6,8 @@ export class ProductsService{
     private products: Product[] = [];
 
     insertProduct(title : string, desc : string, price : number): string {
-        const prodId = new Date().toString();
+        // const prodId = new Date().toString();
+        const prodId = Math.random().toString();
         console.log(prodId, title, desc, price)
         const newProduct = new Product(prodId, title, desc, price);
         this.products.push(newProduct);
@@ -18,12 +19,73 @@ export class ProductsService{
     }
 
     getSingleProduct(productId: string){
-        const product = this.products.find((prod) => prod.id === productId);
+        console.log("productId : ", productId);
+        const product = this.findProduct(productId).product;
+        return {...product};
+    }
 
-        if(!product){
+    updateProduct(
+        productId : string,
+        title : string,
+        desc : string,
+        price : number
+        ){
+            const {product, productIndex} = this.findProduct(productId);
+            // const product = productObject.product;
+            // const index = productObject.productIndex;
+
+            const updateProduct = {...product};
+
+            if(title){
+                updateProduct.title = title;
+            }
+
+            if(desc){
+                updateProduct.desc = desc;
+            }
+
+            if(price){
+                updateProduct.price = price;
+            }
+
+            this.products[productIndex] = updateProduct;
+        }
+
+
+    deleteProduct(productId: string){
+        console.log("deleteProduct : ", productId);
+        // const prodId = this.findProduct(productId).product.id;
+        // console.log("prodId : ",typeof prodId, prodId);
+        
+        // this.products = this.products.filter((ele) => {
+        //     console.log('filter', ele.id, prodId);
+        //     console.log("filter boolean : ", ele.id === prodId )
+        //     return ele.id !== prodId.toString();
+        // });
+
+        const prodId = this.findProduct(productId).productIndex;
+        this.products.splice(prodId, 1);
+
+        return null;
+    }
+    
+    private findProduct(id : string){
+
+        console.log('findProduct ID : ', id);
+
+        // 해당 id를 갖는 상품이 있는지 확인한다.
+        const productIndex = this.products.findIndex((prod) => prod.id === id);
+        console.log("productIndex : ", productIndex);
+        if(productIndex < 0){
+            console.log("no product");
             throw new NotFoundException('Could not find product.');
         }
 
-        return {...product};
+        const product = this.products[productIndex];
+
+        return {product, productIndex};
     }
+
+
 }
+
